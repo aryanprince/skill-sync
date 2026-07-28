05a27c506660fe77f2cfd8340b7f146680a3acdc
Aryan Prince <aryanprincesanty@icloud.com>
2026-07-29 02:35:06 +0530

    chore: scaffold Xcode project

---
+++ SkillSync/SkillSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillSyncApp.swift
+//  SkillSync
+//
+//  Created by Aryan on 29/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SkillSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
